c27c11883f0d1fda13e49d8c2a0389b5fa15449e
cosmic-ankit <76097952+cosmic-ankit@users.noreply.github.com>
2022-10-29 13:54:38 +0530

    Add files via upload

---
+++ js/navbar.js
@@ -0,0 +1,6 @@
+var tog=document.getElementById("dropdowntoggle");
+tog.style.display="none";
+function toggle(){
+    if (tog.style.display=="none") tog.style.display="block";
+    else tog.style.display="none";
+}
